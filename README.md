28f0870b8f8a84a92aae3b49fb2afc8956a7d34d
Di Weng <mystery.wd@gmail.com>
2018-07-21 14:11:11 +0800

    rewrite with bash

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DNSPOD hook for `dehydrated`
 
-This a hook for [dehydrated](https://github.com/lukas2511/dehydrated) (a [Let's Encrypt](https://letsencrypt.org/) ACME client) that allows you to use [DNSPod](https://www.dnspod.cn/) DNS records to respond to `dns-01` challenges. Requires Python and your DNSPod account API token being in the environment.
+This a hook for [dehydrated](https://github.com/lukas2511/dehydrated) (a [Let's Encrypt](https://letsencrypt.org/) ACME client) that allows you to use [DNSPod](https://www.dnspod.cn/) DNS records to respond to `dns-01` challenges. Requires your DNSPod account API token being in the environment.
 
 ## Installation
 
@@ -8,15 +8,8 @@ This a hook for [dehydrated](https://github.com/lukas2511/dehydrated) (a [Let's 
 $ git clone https://github.com/lukas2511/dehydrated
 $ cd dehydrated
 $ mkdir hooks
-$ git clone https://github.com/ftao/letsencrypt-dnspod-hook hooks/dnspod
-$ pip install -r hooks/dnspod/requirements.txt
+$ git clone https://github.com/w1ndy/dehydrated-dnspod-shell-hook.git hooks/dnspod-shell
 ```
-If using Python 2, replace the last step with the one below and check the [urllib3 documentation](http://urllib3.readthedocs.org/en/latest/security.html#installing-urllib3-with-sni-support-and-certificates) for other possible caveats.
-
-```
-$ pip install -r hooks/dnspod/requirements-python-2.txt
-```
-
 
 ## Configuration
 
@@ -26,41 +19,15 @@ Your account's DNSPod LOGIN Token are expected to be in the environment, so make
 $ export DNSPOD_LOGIN_TOKEN='YOUR-DNSPOD-LOGIN-TOKEN'
 ```
 
-
 Alternatively, these statements can be placed in `dehydrated/config.sh`, which is automatically sourced by `dehydrated` on startup:
 
 ```
 echo "export DNSPOD_LOGIN_TOKEN='YOUR-DNSPOD-LOGIN-TOKEN'" >> config.sh
 ```
 
-
-
 ## Usage
 
 ```
-$ ./dehydrated -c -d example.com -t dns-01 -k 'hooks/dnspod/hook.py'
-#
-# !! WARNING !! No main config file found, using default config!
-#
-Processing example.com
- + Signing domains...
- + Creating new directory /home/user/dehydrated/certs/example.com ...
- + Generating private key...
- + Generating signing request...
- + Requesting challenge for example.com...
- + DNSPod hook executing: deploy_challenge
- + DNS not propagated, waiting 30s...
- + DNS not propagated, waiting 30s...
- + Responding to challenge for example.com...
- + DNSPod hook executing: clean_challenge
- + Challenge is valid!
- + Requesting certificate...
- + Checking certificate...
- + Done!
- + Creating fullchain.pem...
- + DNSPod hook executing: deploy_cert
- + ssl_certificate: /home/user/dehydrated/certs/example.com/fullchain.pem
- + ssl_certificate_key: /home/user/dehydrated/certs/example.com/privkey.pem
- + Done!
+$ ./dehydrated -c -d example.com -t dns-01 -k 'hooks/dnspod-shell/hook.sh'
 ```
 
---
+++ hook.sh
@@ -0,0 +1,141 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+ACME_DNS_PREFIX=_acme-challenge
+DNS_SERVER=114.114.114.114
+
+stderr() {
+  >&2 echo "$@"
+}
+
+succeeded() {
+  local code=$(echo $1 | grep -Po "(?<=\"code\":\").*?(?=\")" || true)
+  if [[ ! "$code" == "1" ]]; then
+    stderr " ! DNSPod Error: $(echo $1 | grep -Po "(?<=\"message\":\").*?(?=\")")"
+    return 1
+  fi
+  return 0
+}
+
+check_login_token() {
+  if [[ -z "$DNSPOD_LOGIN_TOKEN" ]]; then
+    stderr " ! No DNSPOD_LOGIN_TOKEN specified"
+    exit 1
+  fi
+  
+  local resp
+  resp="$(curl -s -X POST https://dnsapi.cn/Domain.List -d "login_token=$DNSPOD_LOGIN_TOKEN&format=json")"
+  if ! succeeded "$resp"; then
+    stderr " ! Cannot fetch registered domains"
+  fi
+
+  DOMAINS=$(echo $resp | grep -Po "(?<=\"name\":\").*?(?=\")")
+}
+
+parse_tld() {
+  local domain="$1"
+  for d in $DOMAINS; do
+    if [[ "$domain" =~ "$d" ]]; then
+      echo $d
+      return 0
+    fi
+  done
+  return 1
+}
+
+parse_subdomain() {
+  local domain="$1"
+  local tld
+  tld="$(parse_tld "$domain")"
+  if [[ $? -ne 0 ]]; then return 1; fi
+  echo $domain | grep -Po "^.*(?=\.$tld)"
+}
+
+execute_dnspod_action() {
+  local action="$1"
+  local domain="$2"
+  local params="$3"
+
+  local tld
+  local subdomain
+
+  tld="$(parse_tld "$domain")"
+  if [[ $? -ne 0 ]]; then
+    stderr " ! DNSPod: Domain $domain not found"
+    return 1
+  fi
+
+  subdomain="$(parse_subdomain "$domain")"
+  if [[ $? -ne 0 ]]; then
+    stderr " ! DNSPod: Cannot parse subdomain from domain $domain"
+    return 1
+  fi
+
+  stderr " + DNSPod: Executing $action on $subdomain for $tld..."
+  local resp
+  resp="$(curl -s -X POST "https://dnsapi.cn/$action" -d "login_token=$DNSPOD_LOGIN_TOKEN&domain=$tld&sub_domain=$subdomain&format=json$params")"
+  if ! succeeded "$resp"; then return 1; fi
+
+  echo $resp
+}
+
+deploy_challenge() {
+  local domain="$ACME_DNS_PREFIX.$1"
+  local token="$3"
+  local result
+  local rid
+
+  stderr " + DNSPod: Deploying challenge token $token..."
+  result="$(execute_dnspod_action "Record.Create" $domain "&record_type=TXT&record_line=默认&value=$token")"
+  if [[ $? -ne 0 ]]; then return 1; fi
+  rid="$(echo $result | grep -Po "(?<=\"id\":\").*?(?=\")")"
+  stderr " + DNSPod: TXT record created, ID $rid"
+  
+  while : ; do
+    stderr " + DNSPod: Waiting 10 sec..."
+    sleep 10
+    local records
+    stderr " + DNSPod: Resolving TXT records for domain $domain..."
+    records=$(dig @$DNS_SERVER TXT +short $domain)
+    if [[ $? -ne 0 ]]; then
+      stderr " ! DNSPod: Failed to resolve TXT records for domain $domain"
+      return 1
+    fi
+    for rec in $records; do
+      stderr " + DNSPod: Processing record $rec..."
+      if [[ "$rec" =~ "$token" ]]; then
+	stderr " + DNSPod: Done deploying challenge"
+        return 0
+      fi
+    done
+  done
+}
+
+clean_challenge() {
+  local domain="$ACME_DNS_PREFIX.$1"
+  local token="$3"
+  stderr " + DNSPod: Cleaning challenge token..."
+  
+  local result
+  result="$(execute_dnspod_action Record.List $domain "&record_type=TXT&keyword=$token")"
+  if [[ $? -ne 0 ]]; then return 1; fi
+  
+  local ids
+  ids="$(echo $result | grep -Po "(?<=\"id\":\").*?(?=\")" | tail -n +2)"
+  for id in $ids; do
+    echo " + DNSPod: Removing record $id..."
+    execute_dnspod_action Record.Remove $domain "&record_id=$id" >/dev/null
+  done
+}
+
+main() {
+  check_login_token
+  HANDLER="$1"; shift
+  if [[ "$HANDLER" =~ ^(deploy_challenge|clean_challenge)$ ]]; then
+    "$HANDLER" $@
+  fi
+}
+
+main $@
+
